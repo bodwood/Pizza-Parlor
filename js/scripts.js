9e7a78fb4f2d3c949d7991de7e32aef6d9e39162
bodwood <bodiewood@hotmail.com>
2022-11-04 14:05:48 -0700

    add functionality to choose toppings of pizza using checkboxes.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 //Business Logic ------------------------------------
 
 //Pizza constructor
-function Pizza(toppings = [], size){
+function Pizza(toppings = [], size) {
   this.toppings = toppings;
   this.size = size;
 }
 
 //Places a price on each pizza size
-Pizza.prototype.sizePrices = function(size) {
+Pizza.prototype.sizePrices = function (size) {
   this.size = size;
   let price = 0;
-  switch(this.size){
+  switch (this.size) {
     case "small":
       price = 5;
       return price;
@@ -29,43 +29,44 @@ Pizza.prototype.sizePrices = function(size) {
 };
 
 //Adds .25 for each additional toppings, past two toppings
-Pizza.prototype.addAdditionalToppings = function(toppings) {
+Pizza.prototype.addAdditionalToppings = function (toppings) {
   this.toppings = toppings;
-  if(toppings.length > 2){
+  if (toppings.length > 2) {
     let extraToppings = toppings.length - 2;
     extraToppings *= .25;
     return extraToppings;
-  }else{
+  } else {
     let noExtraCharge = 0;
-    //console.log(noExtraCharge);
     return noExtraCharge;
   }
 };
 
-Pizza.prototype.totalPizzaPrice = function(toppings, size){
+Pizza.prototype.totalPizzaPrice = function (toppings, size) {
   this.toppings = toppings;
   this.size = size;
-  let pizzaSize = pizza.sizePrices(newPizza.size);
-  let additionalToppings = pizza.addAdditionalToppings(newPizza.toppings);
+  let pizzaSize = pizza.sizePrices(size);
+  let additionalToppings = pizza.addAdditionalToppings(toppings);
   return pizzaSize + additionalToppings;
 };
 
 let pizza = new Pizza();
-const newPizza = new Pizza(["pineapple", "ham", "peppers", "onions"], "large");
-pizza.sizePrices(newPizza.size);
-pizza.addAdditionalToppings(newPizza.toppings);
-let totalPrice = pizza.totalPizzaPrice(newPizza.size, newPizza.toppings);
-console.log(totalPrice);
 
 
 //UI Logic --------------------------------------
-function showTotalPrice(event){
+function showTotalPrice(event) {
   event.preventDefault();
   const pizzaSize = document.querySelector('input[name="size"]:checked').value;
-  console.log(pizzaSize);
+  const checkboxes = document.querySelectorAll('input[name="toppings"]:checked');
+  let checkboxValues = [];
+  checkboxes.forEach(function (checkbox) {
+    checkboxValues.push(checkbox.value);
+  });
+  const newPizza = new Pizza(checkboxValues, pizzaSize);
+  console.log(pizza.totalPizzaPrice(newPizza.toppings, newPizza.size));
+
 }
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   const submitButton = document.getElementById("submitBtn");
   submitButton.addEventListener("click", showTotalPrice);
 });
